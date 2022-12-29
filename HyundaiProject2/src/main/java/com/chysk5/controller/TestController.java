65bdf3a1c9757dc379699f7368c6102c2e940bfb
kang <gkatprkd@naver.com>
2022-12-29 12:17:18 +0900

    1229 kang DB연결

---
+++ HyundaiProject2/src/main/java/com/chysk5/controller/TestController.java
@@ -0,0 +1,38 @@
+package com.chysk5.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.chysk5.domain.TestDTO;
+import com.chysk5.service.TestService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@Controller
+@RequestMapping("/test/")
+@RequiredArgsConstructor
+public class TestController {
+	
+	private final TestService service;
+	
+	@GetMapping("/data")
+	public String testdatabase(Model model) {
+		log.info(">>>>>>/data 호출");
+		
+		List<TestDTO> list = service.getDB();
+		
+		model.addAttribute("board",list);
+		
+		log.info(model);
+		
+		return "check";
+	}
+	
+}
---
+++ HyundaiProject2/src/main/java/com/chysk5/service/TestService.java
@@ -0,0 +1,11 @@
+package com.chysk5.service;
+
+import java.util.List;
+
+import com.chysk5.domain.TestDTO;
+
+public interface TestService {
+	
+	public List<TestDTO> getDB();
+	
+}
