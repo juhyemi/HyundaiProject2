1460da1585aabae73c53bab4c014b203d0f85948
kang <gkatprkd@naver.com>
2023-01-04 09:10:31 +0900

    0104 resell 상세

--- HyundaiProject2/src/main/java/com/chysk5/domain/ResellProductDetailInfoDTO.java
+++ HyundaiProject2/src/main/java/com/chysk5/domain/ResellProductDetailInfoDTO.java
@@ -2,11 +2,21 @@ package com.chysk5.domain;
 
 import java.util.List;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Getter
+@Setter
+@ToString
 public class ResellProductDetailInfoDTO {
 	private String pro_name;
 	private String pro_id;
+	private String pro_category;
+	private String pro_price;
 	
-	private List<ResellProductSizeDTO> sizeImg; 
+	private List<ResellProductSizeDTO> listSize; 
 	private List<ResellProductImgDTO> listImg;
 	
 }
--- HyundaiProject2/src/main/java/com/chysk5/mapper/ResellMapper.java
+++ HyundaiProject2/src/main/java/com/chysk5/mapper/ResellMapper.java
@@ -8,8 +8,10 @@ import com.chysk5.domain.PriceRankDTO;
 import com.chysk5.domain.RegResellProductDTO;
 import com.chysk5.domain.ResellPriceDTO;
 import com.chysk5.domain.ResellProductDTO;
+import com.chysk5.domain.ResellProductDetailInfoDTO;
 import com.chysk5.domain.ResellProductImgDTO;
 import com.chysk5.domain.ResellProductListDTO;
+import com.chysk5.domain.ResellProductSizeDTO;
 
 public interface ResellMapper {
 
@@ -20,8 +22,14 @@ public interface ResellMapper {
    public int getMyRank(@Param("pro_opt_id") String pro_opt_id, @Param("re_price") int re_price);
    public int existData(@Param("pro_opt_id") String pro_opt_id, @Param("re_price") int re_price);
 
-
-	public List<ResellProductListDTO> getResellProductList();
-	public List<ResellProductImgDTO> getResellProductListImg(String proId);
+	/* Resell 상품 리스트 불러오기 */
+   public List<ResellProductListDTO> getResellProductList();
+   public List<ResellProductImgDTO> getResellProductListImg(String proId);
+   
+	/* Resell 상품 상세 불러오기 */
+   public ResellProductDetailInfoDTO getResellProductDetail(String proId);
+   public List<ResellProductImgDTO> getResellProductDetailImgs(String proId);
+   public List<ResellProductSizeDTO> getResellProductDetailSizes(String proId);
+   
 }
 
