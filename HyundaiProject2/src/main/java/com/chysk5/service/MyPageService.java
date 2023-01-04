02a455b2bd63aa16b3e405c55a73547be26f26a0
Kieeary <70502804+Kieeary@users.noreply.github.com>
2023-01-04 09:15:51 +0900

    0104 기범 mypageresell 수정

--- HyundaiProject2/src/main/java/com/chysk5/service/MyPageService.java
+++ HyundaiProject2/src/main/java/com/chysk5/service/MyPageService.java
@@ -6,5 +6,5 @@ import com.chysk5.domain.MyResellProductDTO;
 public interface MyPageService {
 	
 	public List<MyResellProductDTO> getMyResellList(String mem_id);
-
+	public int removeMyResellProduct(String re_id);
 }
